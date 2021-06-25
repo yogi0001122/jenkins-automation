688214ae382c0ed988f479fd09cb0f85cd28b3e8
yogi0001122 <56965623+yogi0001122@users.noreply.github.com>
2021-06-25 10:06:30 +0530

    Update sample1.groovy

--- sample1.groovy
+++ sample1.groovy
@@ -1,7 +1,6 @@
 pipeline {
     agent { label 'master' }
     parameters {
-        string(name: 'command', defaultValue: 'get nodes', description: 'kubectl command to run, will be kubectl $command, will print out.txt if it is created')
         string(name: 'environment_name', description: 'Environment to deploy into')
     }
     stages {
